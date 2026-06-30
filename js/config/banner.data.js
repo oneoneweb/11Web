d7ecb17159be8f66da5aeeba9b1e1c4460f1d59b
oneoneweb <casinobuzzbd@gmail.com>
2026-06-30 15:20:23 +0600

    Add files via upload

---
+++ js/config/banner.data.js
@@ -0,0 +1,38 @@
+window.BANNER_DATA = [
+  {
+    image: "assets/banners/wicket71.webp",
+    siteName: "Wicket71",
+    link: "https://qi8no.wicket71bd.com/register.html",
+    buttonText: "Play Now"
+  },
+  {
+    image: "assets/banners/jeetbuzz.webp",
+    siteName: "JeetBuzz",
+    link: "https://jb2236.com/af/PRMyf6oe/join",
+    buttonText: "Play Now"
+  },
+  {
+    image: "assets/banners/jeeta.webp",
+    siteName: "Jeeta",
+    link: "https://cfjrz.jeeta77.com/register.html",
+    buttonText: "Play Now"
+  },
+  {
+    image: "assets/banners/banglawin.webp",
+    siteName: "BanglaWin",
+    link: "https://bnwnaff332244.com/af/mBHB9t4p/11web",
+    buttonText: "Play Now"
+  },
+  {
+    image: "assets/banners/crickex.webp",
+    siteName: "Crickex",
+    link: "https://cxsport.vip/af/JrTtt3zu/join",
+    buttonText: "Play Now"
+  },
+  {
+    image: "assets/banners/deshislots.webp",
+    siteName: "Deshi Slots",
+    link: "https://desh8slotaff888.com/af/04ZI5wd2/11web",
+    buttonText: "Play Now"
+  }
+];
---
+++ js/config/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAdaZGQA8tLgzKdZ...",
+  authDomain: "web11-one.firebaseapp.com",
+  projectId: "web11-one",
+  storageBucket: "web11-one.firebasestorage.app",
+  messagingSenderId: "88725708371",
+  appId: "1:88725708371:web:dfd08ca43262017708f0a6",
+  measurementId: "G-70Z75SEEQP"
+};
+
+// 🔥 INIT FIREBASE (CRITICAL)
+firebase.initializeApp(firebaseConfig);
